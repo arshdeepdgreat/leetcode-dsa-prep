54125680ab3a51d5067463e03367934d3baf7d3e
Arshdeep Singh Bhatia <59660208+arshdeepdgreat@users.noreply.github.com>
2022-07-31 16:27:16 +0530

    Time: 19 ms (68.45%), Space: 9.2 MB (61.33%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int subset(int n,vector<int>nums,int sum)  {
         int dp[n+1][sum+1];
-        for(int i=0;i<=n;i++)
+        for(int i=0;i<n+1;i++){
             dp[i][0]=1;
-        for(int j=1;j<=sum;j++)
+        }
+        for(int j=1;j<sum+1;j++){
             dp[0][j]=0;
+        }
         for(int i=1;i<=n;i++){
             for(int j=0;j<=sum;j++){
                 dp[i][j]=dp[i-1][j];
